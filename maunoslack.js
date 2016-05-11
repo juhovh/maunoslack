4847cc74c1a22897493663a91fee982f1e53ab96
Juho Vähä-Herttua <juhovh@iki.fi>
2016-05-11 18:34:08 +0300

    Add support for converting private image links to public.

--- maunoslack.js
+++ maunoslack.js
@@ -1,9 +1,58 @@
 var irc = require('irc');
+var fetch = require('node-fetch');
 var servers = require('./config.json').servers;
+
+fetch.Promise = require('es6-promise').Promise;
+
+function getFilePublicUrl(slackToken, fileid) {
+  var url = 'https://slack.com/api/files.info';
+  url += '?token=' + encodeURIComponent(slackToken);
+  url += '&file=' + encodeURIComponent(fileid);
+  return fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      if (json && json.file && json.file.public_url_shared) {
+        return json.file.permalink_public;
+      }
+    });
+}
+
+function makeFilePublic(slackToken, fileid) {
+  var url = 'https://slack.com/api/files.sharedPublicURL';
+  url += '?token=' + encodeURIComponent(slackToken);
+  url += '&file=' + encodeURIComponent(fileid);
+  return fetch(url);
+}
 
 function setupListeners(fromConfig, toConfig) {
   fromConfig.client.addListener('message', function(from, to, message) {
     if (from === fromConfig.nick) return;
+
+    if (fromConfig.slackToken) {
+      var linkre = new RegExp('https://[^\.]+\.slack.com/files/[^\.]+/([^\.]+)/[a-zA-Z0-9\.-_~!$&\'()*+,;=:@]+');
+      if (message && linkre.test(message)) {
+        var fileid = message.match(linkre)[1];
+        makeFilePublic(fromConfig.slackToken, fileid)
+          .then(function() {
+            return getFilePublicUrl(fromConfig.slackToken, fileid);
+          }).then(function(publicUrl) {
+            if (publicUrl) {
+              return publicUrl;
+            } else {
+              throw new Error('Public URL not found');
+            }
+          }).then(function(publicUrl) {
+            // Send updated message with the public URL instead
+            message = message.replace(linkre, publicUrl);
+            toConfig.client.say(toConfig.channel, '<' + from + '> ' + message);
+          }).catch(function(error) {
+            // Fallback to original message if something went wrong
+            toConfig.client.say(toConfig.channel, '<' + from + '> ' + message);
+          });
+        return;
+      }
+    }
 
     toConfig.client.say(toConfig.channel, '<' + from + '> ' + message);
   });
